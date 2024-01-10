bbddfe880626cd497f2929087149dec9a35d71bc
Alejandro Lobato Haurie <42q21870ece4a7dig0peszeylzp6np@bots.bitbucket.org>
2024-01-10 13:15:30 +0100

    THEB-4-frontend-footer Alejandro Lobato

--- client/src/layouts/RootLayout.jsx
+++ client/src/layouts/RootLayout.jsx
@@ -1,1 +1,34 @@
+import { Outlet } from 'react-router-dom'
 
+import { Container } from '@mui/material'
+import { Footer } from 'components'
+
+// import { Navbar } from 'components'
+import { ToastContainer } from 'react-toastify'
+function RootLayout() {
+  return (
+    <>
+      <Container display='flex' maxWidth='xxl' disableGutters>
+        {/* <Navbar /> */}
+        <Container maxWidth='xl' sx={{ mt: 5 }}>
+          <Outlet />
+        </Container>
+
+        <ToastContainer
+          position='top-right'
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme='colored'
+        />
+      </Container>
+      <Footer />
+    </>
+  )
+}
+export default RootLayout
