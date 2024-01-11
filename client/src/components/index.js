03ae2a9af2f083b394f6128ce4f41b51d4a16478
Alberto Lobato Oliver <42q21870ece4a7dig0peszeylzp6np@bots.bitbucket.org>
2024-01-11 09:34:43 +0100

    Pagina principal y carta - Alberto Lobato

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,1 +1,2 @@
 export { default as Footer } from './Footer/footer'
+// export {default as Navbar } from './Navbar/index'
--- client/src/layouts/RootLayout.jsx
+++ client/src/layouts/RootLayout.jsx
@@ -3,11 +3,12 @@ import { Container } from '@mui/material'
 import { Footer } from 'components'
 import { ToastContainer } from 'react-toastify'
 
+
 function RootLayout() {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
       <Container display='flex' flexDirection='column' maxWidth='xxl' disableGutters style={{ flex: 1 }}>
-        {/* <Navbar /> */}
+         {/* <Navbar /> */}
         <Container>
           <Outlet />
         </Container>
