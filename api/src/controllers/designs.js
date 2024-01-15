638eade174036c86d2c3893c9846b24e824b8ec7
David Guerrero <42q21870ece4a7dig0peszeylzp6np@bots.bitbucket.org>
2024-01-15 09:04:41 +0100

    nodemailer added on post design David Guerrero

--- api/src/controllers/designs.js
+++ api/src/controllers/designs.js
@@ -1,6 +1,7 @@
 const { Design } = require('../models/design')
 const mongoose = require('mongoose')
 const { Vote } = require('../models/vote')
+const { sendEmail } = require('../utils/nodemailer')
 
 const getAll = async (req, res) => {
   try {
@@ -43,6 +44,8 @@ const getById = async (req, res) => {
 const create = async (req, res) => {
   try {
     const userID = req.user.id
+    const email = req.user.email
+    const username = req.user.username
 
     const { path: image, filename: imageCloudinaryId } = req.file
 
@@ -51,7 +54,14 @@ const create = async (req, res) => {
       image,
       imageCloudinaryId,
       author: new mongoose.Types.ObjectId(userID),
+      uploadDate: new Date(),
     })
+
+    console.log(req.user)
+
+    if (newDesign) {
+      await sendEmail(email, username)
+    }
 
     res.json(newDesign)
   } catch (error) {
---
+++ api/src/utils/nodemailer.js
@@ -0,0 +1,27 @@
+const nodemailer = require('nodemailer')
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.NODEMAILER_EMAIL,
+    pass: process.env.NODEMAILER_KEY,
+  },
+})
+
+const sendEmail = (email, username) => {
+  transporter.sendMail({
+    from: '"Concursos Pampling" <proyectopampling@gmail.com>',
+    to: `${email}`,
+    subject: 'Diseño enviado',
+    html: `
+    <b>Hola ${username}!</b> 
+    <br>
+    <p>Tu diseño ha sido subido con éxito, si los administradores consideran que cumple con los requisitos adecuados será publicado en nuestra web.</p>
+    <br>
+    <p>Gracias por participar!.</p>`,
+  })
+}
+
+exports.sendEmail = sendEmail
