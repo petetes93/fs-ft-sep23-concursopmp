2b38fe40cd0008d872b2207bc427f82151102f78
Marcos Lemus Conejo <42q21870ece4a7dig0peszeylzp6np@bots.bitbucket.org>
2024-01-16 20:51:30 +0100

    Navbar actualizado y acabado by Marcos Lemus

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -1,45 +1,46 @@
-import React from 'react'
-import { createBrowserRouter } from 'react-router-dom'
-import ErrorPage from 'pages/ErrorPage'
-import RootLayout from 'layouts/RootLayout'
-import ProductsPage from './pages/ProductsPage'
-import ContestPage from './pages/ContestPage'
-import LoginPage from 'pages/LoginPage'
-import RegisterPage from 'pages/RegisterPage'
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "pages/ErrorPage";
+import RootLayout from "layouts/RootLayout";
+import ProductsPage from "./pages/ProductsPage";
+import ContestPage from "./pages/ContestPage";
+import LoginPage from "pages/LoginPage";
+import RegisterPage from "pages/RegisterPage";
 // import AdminPage from "./pages/AdminPage";
-import AdminUserPage from './pages/AdminUserPage'
-import DetailsPage from './pages/DetailPage'
-import LoginAdmin from './pages/AdminLogin'
-import ConcursoForm from './pages/CrearConcurso/crearconcurso'
-import AddDesingPage from 'pages/AddDesingPage'
-import AdminContestPage from './pages/AdminContestPage'
-import ProtectedRoute from './utils/ProtectedRoute'
+import AdminUserPage from "./pages/AdminUserPage";
+import DetailsPage from "./pages/DetailPage";
+import LoginAdmin from "./pages/AdminLogin";
+import ConcursoForm from "./pages/CrearConcurso/crearconcurso";
+import AddDesingPage from "pages/AddDesingPage";
+import AdminContestPage from "./pages/AdminContestPage";
+import ProtectedRoute from "./utils/ProtectedRoute";
+import LogoutPage from "./pages/LogoutPage";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <RootLayout />,
     errorElement: <ErrorPage />,
     children: [
       {
-        path: '/',
+        path: "/",
         element: <ContestPage />,
       },
       {
-        path: '/contest/:contestId',
+        path: "/contest/:contestId",
         element: <ProductsPage />,
       },
       {
-        path: '/product/add_desing',
+        path: "/product/add_desing",
         element: <AddDesingPage />,
       },
       {
-        path: '/login',
+        path: "/login",
         // element: <ProtectedRoute page={LoginPage} role='anonymous' />,
         element: <LoginPage />,
       },
       {
-        path: '/register',
+        path: "/register",
         // element: <ProtectedRoute page={RegisterPage} role='anonymous' />,
         element: <RegisterPage />,
       },
@@ -49,29 +50,33 @@ const router = createBrowserRouter([
       //   element: <AdminPage />,
       // },
       {
-        path: '/adminuser',
+        path: "/adminuser",
         // element: <ProtectedRoute page={RegisterPage} role='anonymous' />,
         element: <AdminUserPage />,
       },
       {
-        path: '/admincontest',
+        path: "/admincontest",
         // element: <ProtectedRoute page={RegisterPage} role='anonymous' />,
         element: <AdminContestPage />,
       },
       {
-        path: '/design/:designId',
+        path: "/design/:designId",
         element: <DetailsPage />,
       },
       {
-        path: '/admin',
+        path: "/admin",
         element: <LoginAdmin />,
       },
       {
-        path: '/createcontest',
+        path: "/createcontest",
         element: <ConcursoForm />,
+      },
+      {
+        path: "/logout",
+        element: <LogoutPage />,
       },
     ],
   },
-])
+]);
 
-export default router
+export default router;
